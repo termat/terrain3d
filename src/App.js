9730e5e4b14759755949a43e305c42afd4d6a40a
termat <ter.matsuoka@gmail.com>
2021-10-21 02:41:26 +0900

    update

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ const dirLight = new SunLight({
 const mvt=()=>{
   const layer = new MVTLayer({
     id:"mvt",
-    data: `%PUBLIC_URL%/pbf/{z}/{x}/{y}.pbf`,
+    data: `https://termat.github.io/terrain3d/pbf/{z}/{x}/{y}.pbf`,
     minZoom: 12,
     maxZoom: 16,
     getLineColor: [192, 192, 192],
